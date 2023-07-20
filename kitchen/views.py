e48afe08176f86cc423d4c3658cef24aa45b1876
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 16:41:48 +0300

    Using sessions create visits counter on home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -10,10 +10,14 @@ def index(request):
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
 
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_dishes": num_dishes,
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1
     }
 
     return render(request, "kitchen/index.html", context=context)
