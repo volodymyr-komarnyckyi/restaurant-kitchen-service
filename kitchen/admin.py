8d72e8e65c00790329ea8cff74641c216d90ad48
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 12:54:53 +0300

    Add years of experience for Cook. Add models to admin panel. Add search and filter in admin panel for Dish model. Modified Cook model in admin page (changed display list, fieldsets and add_fieldsets)

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import DishType, Dish, Cook
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "dish_type"]
+    list_filter = ["dish_type"]
+    search_fields = ["name"]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "years_of_experience",)}),
+    )
+
+
+admin.site.register(DishType)
--- kitchen/models.py
+++ kitchen/models.py
@@ -14,9 +14,13 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
 
     class Meta:
         ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Dish(models.Model):
