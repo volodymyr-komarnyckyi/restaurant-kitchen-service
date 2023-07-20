63bab5d84c60f7b4b99bffe5c0e5fa5e160c3cdd
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 22:24:23 +0300

    Create login page using django built-in authentication

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,10 +1,12 @@
-from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
 from kitchen.models import Dish, Cook, DishType
 
 
+@login_required
 def index(request):
     num_dishes = Dish.objects.count()
     num_cooks = Cook.objects.count()
@@ -23,26 +25,26 @@ def index(request):
     return render(request, "kitchen/index.html", context=context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dish_type_list"
     template_name = "kitchen/dish_type_list.html"
     paginate_by = 2
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
     paginate_by = 2
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
 
 
-class CookDetailView(generic.DetailView):
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
