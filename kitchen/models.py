1cc5ae3c823ab04f57d8bef9f19d29dbe2cbd56f
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 12:05:19 +0300

    Add base models for restaurant kitchen service

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+
+    class Meta:
+        ordering = ["username"]
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, dish type: {self.dish_type.name})"
