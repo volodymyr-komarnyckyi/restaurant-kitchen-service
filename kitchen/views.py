befe1ba55c1cd3223ca54eaa25f06a7a271bd5e5
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 13:18:41 +0300

    Add basic home page for restaurant

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(
+        "<html>"
+        "<h1>Welcome to Restaurant!</h1>"
+        "</html>"
+    )
