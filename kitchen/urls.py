7dcac3d4c141dafe2d4e76d96a7ec9991bdc08fa
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-20 15:42:24 +0300

    Create Class-based views (solved N+1 problem)

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishListView, CookListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list"
+    ),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list"
+    ),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, Cook, DishType
 
@@ -15,3 +16,18 @@ def index(request):
     }
 
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "kitchen/dish_type_list.html"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    queryset = Dish.objects.select_related("dish_type")
+
+
+class CookListView(generic.ListView):
+    model = Cook
