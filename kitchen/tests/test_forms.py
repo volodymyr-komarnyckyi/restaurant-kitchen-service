b0f9202d76f1852153018b18d009fc21ee38ac3e
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-07-23 15:45:35 +0300

    Add tests for models and forms

---
+++ kitchen/tests/test_forms.py
@@ -0,0 +1,124 @@
+from django.test import TestCase
+
+from kitchen.models import DishType, Cook
+from kitchen.forms import (
+    CookCreationForm,
+    DishForm,
+    CookSearchForm,
+    DishSearchForm,
+    DishTypeSearchForm
+)
+
+
+class CookCreationFormTest(TestCase):
+    def test_valid_form(self):
+        form_data = {
+            "username": "testuser",
+            "password1": "testpassword123",
+            "password2": "testpassword123",
+            "years_of_experience": 5,
+            "first_name": "John",
+            "last_name": "Doe",
+            "photo": None,
+        }
+        form = CookCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            "username": "",
+            "password1": "testpassword123",
+            "password2": "testpassword123",
+            "years_of_experience": 5,
+            "first_name": "John",
+            "last_name": "Doe",
+            "photo": None,
+        }
+        form = CookCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class DishFormTest(TestCase):
+    def test_valid_form(self):
+        dish_type = DishType.objects.create(name="Main Course")
+        cook1 = Cook.objects.create(
+            username="cook1",
+            first_name="John",
+            last_name="Doe"
+        )
+        cook2 = Cook.objects.create(
+            username="cook2",
+            first_name="Tim",
+            last_name="Black"
+        )
+
+        form_data = {
+            "name": "Delicious Dish",
+            "description": "A tasty dish",
+            "price": 10.99,
+            "dish_type": dish_type.id,
+            "cooks": [cook1.id, cook2.id],
+            "dish_photo": None,
+        }
+        form = DishForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            "name": "",
+            "description": "A tasty dish",
+            "price": 10.99,
+            "dish_type": 1,
+            "cooks": [1, 2],
+            "dish_photo": None,
+        }
+        form = DishForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class CookSearchFormTest(TestCase):
+    def test_valid_form(self):
+        form_data = {
+            "last_name": "Doe",
+        }
+        form = CookSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            "last_name": "",
+        }
+        form = CookSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class DishSearchFormTest(TestCase):
+    def test_valid_form(self):
+        form_data = {
+            "name": "Delicious Dish",
+        }
+        form = DishSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            "name": "",
+        }
+        form = DishSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class DishTypeSearchFormTest(TestCase):
+    def test_valid_form(self):
+        form_data = {
+            "name": "Main Course",
+        }
+        form = DishTypeSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            "name": "",
+        }
+        form = DishTypeSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
---
+++ kitchen/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+
+from kitchen.models import DishType, Dish, Cook
+
+
+class DishTypeModelTest(TestCase):
+    def test_str_representation(self):
+        dish_type = DishType.objects.create(name="Main Course")
+        self.assertEqual(str(dish_type), "Main Course")
+
+
+class CookModelTest(TestCase):
+    def test_str_representation(self):
+        cook = Cook.objects.create(
+            username="chef123",
+            first_name="John",
+            last_name="Doe"
+        )
+        self.assertEqual(str(cook), "chef123 (John Doe)")
+
+
+class DishModelTest(TestCase):
+    def test_str_representation(self):
+        dish_type = DishType.objects.create(name="Dessert")
+        cook = Cook.objects.create(
+            username="baker12",
+            first_name="Alice",
+            last_name="Smith"
+        )
+        dish = Dish.objects.create(
+            name="Chocolate Cake",
+            description="Delicious chocolate cake",
+            price="25.00",
+            dish_type=dish_type
+        )
+        dish.cooks.add(cook)
+        self.assertEqual(
+            str(dish),
+            "Chocolate Cake (price: 25.00, dish type: Dessert)"
+        )
